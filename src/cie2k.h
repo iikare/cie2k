b324045bd46eb3f4c1fcafa2dab5c11030eace20
iikare <13582030+iikare@users.noreply.github.com>
2023-05-10 17:56:25 -0700

    fix impl.

--- src/cie2k.h
+++ src/cie2k.h
@@ -13,137 +13,129 @@ T deltaE(T l1, T a1, T b1, T l2, T a2, T b2) {
   // as described in "The CIEDE2000 Color-Difference Formula: Implementation Notes, 
   // Supplementary Test Data, and Mathematical Observations" by Gaurav Sharma, Wencheng Wu, Edul N. Dalal
 
-  // utility lambdas
+  // utility lambda
   constexpr auto to_rad = [&] (const T val) {
     return (val*M_PI)/180.0;
-  };
-  constexpr auto to_deg = [&] (const T val) {
-    return (180.0/M_PI)*val;
   };
 
   // eq.1 omitted
 
   // eq.2
-  T C1 = sqrt((a1*a1)+(b1*b1));
-  T C2 = sqrt((a2*a2)+(b2*b2));
+  const T C1 = sqrt(a1*a1 + b1*b1);
+  const T C2 = sqrt(a2*a2 + b2*b2);
 
   // eq.3
-  T C_bar = (C1+C2)/2.0;
-
+  const T C_bar = (C1+C2)/2.0;
+  
   // eq.4
-  constexpr T eq_25p7 = 6103515625.0;
-  T G = 0.5*(1.0-sqrt(pow(C_bar,7)/(pow(C_bar,7)+eq_25p7)));
-
+  constexpr T eq25p7 = 6103515625.0;
+  const T C_bar_7 = pow(C_bar, 7.0);
+  const T G = 0.5*(1-sqrt(C_bar_7/(C_bar_7+eq25p7)));
+  
   // eq.5
-  T a1_prime = (1.0+G)*a1;
-  T a2_prime = (1.0+G)*a2;
-
+  const T a1_prime = (1.0+G)*a1;
+  const T a2_prime = (1.0+G)*a2;
+  
   // eq.6
-  T C1_prime = sqrt((a1_prime*a1_prime)+(b1*b1));
-  T C2_prime = sqrt((a2_prime*a2_prime)+(b2*b2));
-
+  const T C1_prime = sqrt(a1_prime*a1_prime + b1*b1);
+  const T C2_prime = sqrt(a2_prime*a2_prime + b2*b2);
+  
   // eq.7
   T h1_prime = 0.0;
   if (!(b1 == 0 && a1_prime == 0)) {
     h1_prime = atan2(b1, a1_prime);
-    if (h1_prime < 0.0) {
-      h1_prime += 2*M_PI;
+    if (h1_prime < 0) {
+      h1_prime += to_rad(360.0);
     }
-    h1_prime = to_deg(h1_prime);
   }
   T h2_prime = 0.0;
   if (!(b2 == 0 && a2_prime == 0)) {
-    h2_prime = atan2(b2, a2_prime); 
-    if (h2_prime < 0.0) {
-      h2_prime += 2*M_PI;
+    h2_prime = atan2(b2, a2_prime);
+    if (h2_prime < 0) {
+      h2_prime += to_rad(360.0);
     }
-    h2_prime = to_deg(h2_prime);
   }
-
+  
   // eq.8
-  T delta_L_prime = l2-l1;
-
+  const T delta_L_prime = l2 - l1;
+  
   // eq.9
-  T delta_C_prime = C2_prime-C1_prime;
-
+  const T delta_C_prime = C2_prime - C1_prime;
+  
   // eq.10
   T delta_h_prime = 0;
   if (C1_prime*C2_prime != 0) {
-    T h_diff = h2_prime-h1_prime;
-    //if (fabs(h_diff) <= M_PI) {
-    delta_h_prime = h_diff;
-    //}
-    if (h_diff > M_PI) {
-      delta_h_prime = h_diff - 2*M_PI;
+    delta_h_prime = h2_prime-h1_prime;
+    if (delta_h_prime < -to_rad(180.0)) {
+      delta_h_prime += to_rad(360.0);
     }
-    else {
-    //else if (h_diff < -M_PI) {
-      delta_h_prime = h_diff + 2*M_PI;
+    else if (delta_h_prime > to_rad(180.0)) {
+      delta_h_prime -= to_rad(360.0);
     }
   }
-
+  
   // eq.11
-  T delta_H_prime = 2.0*sqrt(C1_prime*C2_prime)*sin(delta_h_prime/2.0);
-
+  const T delta_H_prime = 2.0*sqrt(C1_prime*C2_prime)*sin(delta_h_prime/2.0);
+  
   // eq.12
-  T L_bar_prime = (l1+l2)/2.0;
-
+  const T L_bar_prime = (l1+l2)/2.0;
+  
   // eq.13
-  T C_bar_prime = (C1_prime+C2_prime)/2.0;
-
+  const T C_bar_prime = (C1_prime+C2_prime)/2.0;
+  
   // eq.14
-  T h_bar_prime = h1_prime+h2_prime;
+  T h_bar_prime = h1_prime + h2_prime;
   if (C1_prime*C2_prime != 0) {
-    if (fabs(h1_prime-h2_prime) <= M_PI) {
-      h_bar_prime = h_bar_prime/2.0;
+    if (fabs(h1_prime-h2_prime) <= to_rad(180.0)) {
+      h_bar_prime = (h1_prime+h2_prime) / 2.0;
     }
     else {
-      if (h1_prime+h2_prime < 2*M_PI) {
-        h_bar_prime = (h_bar_prime+2*M_PI)/2.0;
-      }
+      if (h1_prime+h2_prime < to_rad(360.0)) {
+        h_bar_prime = (h1_prime+h2_prime+to_rad(360.0))/2.0;
+      } 
       else {
-        h_bar_prime = (h_bar_prime-2*M_PI)/2.0;
+        h_bar_prime = (h1_prime+h2_prime-to_rad(360.0))/2.0;
       }
     }
   }
-
+  
   // eq.15
-  T t = 1.0 - 0.17*cos(h_bar_prime-to_rad(30.0))
-            + 0.24*cos(h_bar_prime*2.0)
-            + 0.32*cos(h_bar_prime*3.0+to_rad(6.0))
-            - 0.20*cos(h_bar_prime*4.0)-to_rad(63.0);
-
+  const T t = 1.0 - 0.17*cos(h_bar_prime-to_rad(30.0)) +
+                    0.24*cos(h_bar_prime*2.0) +
+                    0.32*cos(h_bar_prime*3.0+to_rad(6.0)) - 
+                    0.20*cos(h_bar_prime*4.0-to_rad(63.0));
+  
   // eq.16
-  T delta_theta = to_rad(30.0)*exp(-pow((h_bar_prime-to_rad(275.0))/to_rad(25.0),2.0)); 
+  const T delta_theta = to_rad(30.0)*exp(-pow((h_bar_prime-to_rad(275.0)) / to_rad(25.0), 2.0));
 
   // eq.17
-  //T C_bar_prime_7 = pow(C_bar_prime, 7);
-  T R_C = 2*sqrt(pow(C_bar_prime,7)/(pow(C_bar_prime,7)+eq_25p7));
-
+  const T C_bar_prime_7 = pow(C_bar_prime,7.0);
+  const T R_C = 2.0*sqrt(C_bar_prime_7 / (C_bar_prime_7+eq25p7));
+  
   // eq.18
-  T L_50_2 = (L_bar_prime-50)*(L_bar_prime-50);
-  T S_L = 1.0 + (0.015*L_50_2)/sqrt(20+L_50_2);
-
+  const T L_bar_prime_2 = pow(L_bar_prime-50.0,2.0);
+  const T S_L = 1.0 + (0.015*L_bar_prime_2) / sqrt(20.0+L_bar_prime_2);
+  
   // eq.19
-  T S_C = 1.0 + 0.045*C_bar_prime;
-
+  const T S_C = 1.0 + (0.045*C_bar_prime);
+  
   // eq.20
-  T S_H = 1.0 + 0.015*C_bar_prime*t; 
-
+  const T S_H = 1.0 + (0.015*C_bar_prime*t);
+  
   // eq.21
-  T R_T = (-sin(2*delta_theta))*R_C;
-
-  // k_L, k_C, k_H set as chosen in original paper
+  const T R_T = -sin(2.0*delta_theta)*R_C;
+ 
+  // eq.22
+  // constants as defined in the implementation paper
   constexpr T k_L = 1.0;
   constexpr T k_C = 1.0;
   constexpr T k_H = 1.0;
 
-  // eq.22
-  T deltaE = sqrt(pow(delta_L_prime/(k_L*S_L), 2.0) +
-                  pow(delta_C_prime/(k_C*S_C), 2.0) +
-                  pow(delta_H_prime/(k_H*S_H), 2.0) +
-                  R_T*(delta_C_prime/(k_C*S_C))*(delta_H_prime/(k_H*S_H)));
-
+  T deltaE = sqrt(pow(delta_L_prime / (k_L*S_L), 2.0) +
+                  pow(delta_C_prime / (k_C*S_C), 2.0) +
+                  pow(delta_H_prime / (k_H*S_H), 2.0) + 
+                  R_T*delta_C_prime*delta_H_prime/(k_C*S_C*k_H*S_H));
+  
   return deltaE;
 
 }
--- test.cc
+++ test.cc
@@ -1,4 +1,5 @@
 #include <string>
+#include <iomanip>
 #include "src/cie2k.h"
 #include "gen/test.h"
 #include "log.h"
@@ -6,7 +7,7 @@
 bool fp_eq(float a, float b) {
   char a_buf[128], b_buf[128];
 	sprintf(a_buf, "%.4f", a);
-	sprintf(b_buf, "%.4f", a);
+	sprintf(b_buf, "%.4f", b);
   //logQ(a_buf, b_buf);
   return std::string(a_buf) == std::string(b_buf);
 }
@@ -16,26 +17,26 @@ int main() {
   unsigned int test_num = 0;
   unsigned int test_pass = 0;
 
-  // 0:  l1           [OK]
-  // 1:  a1           [OK]
-  // 2:  b1           [OK]
-  // 3:  a1_prime     [OK]
-  // 4:  C1_prime     [OK]
-  // 5:  h1_prime     [OK]
-  // 6:  h_bar_prime  [OK]
-  // 7:  G            [OK]
-  // 8:  T (t)        [OK]
-  // 9:  S_L          [OK]
-  // 10: S_C          [OK]
-  // 11: S_H          [OK]
-  // 12: R_T          [OK] 
-  // 13: deltaE       [OK]
-  // 14: l2           [OK]
-  // 15: a2           [OK] 
-  // 16: b2           [OK]
-  // 17: a2_prime     [OK]
-  // 18: C2_prime     [OK]
-  // 19: h2_prime     [OK]
+  // 0:  l1           []
+  // 1:  a1           []
+  // 2:  b1           []
+  // 3:  a1_prime     []
+  // 4:  C1_prime     []
+  // 5:  h1_prime     []
+  // 6:  h_bar_prime  []
+  // 7:  G            []
+  // 8:  T (t)        []
+  // 9:  S_L          []
+  // 10: S_C          []
+  // 11: S_H          []
+  // 12: R_T          [] 
+  // 13: deltaE       []
+  // 14: l2           []
+  // 15: a2           [] 
+  // 16: b2           []
+  // 17: a2_prime     []
+  // 18: C2_prime     []
+  // 19: h2_prime     []
 
   constexpr unsigned int idx  = 13;
 
@@ -46,9 +47,19 @@ int main() {
     const double expectDE = test_item[idx];
     const double actualDE = cie2k::deltaE(test_item[0] , test_item[1] , test_item[2],
                                           test_item[14], test_item[15], test_item[16]);
+
+    std::ostringstream ss;
+    ss << std::setw(2) << std::setfill('0') << test_num;
+    string s_test_num = ss.str();
+
+    ss.str("");
+    ss << std::fixed << std::setw(9) << std::setfill(' ') << (actualDE - expectDE);
+    string margin = ss.str();
+
+
     bool correct = fp_eq(expectDE, actualDE);
     if (correct) {
-      logW(LL_INFO, "[PASS] - test", test_num);
+      logW(LL_INFO, "[PASS] - test", s_test_num, "| margin: ("+margin+")");
       test_pass++;
     }
     else {
